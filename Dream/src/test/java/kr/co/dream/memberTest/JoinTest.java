186b7dce9474bd9c9ef97c8ff8c6151d299ce871
Cys <rcn115@naver.com>
2017-01-13 15:08:31 +0900

    login TEST

--- Dream/src/test/java/kr/co/dream/memberTest/JoinTest.java
+++ Dream/src/test/java/kr/co/dream/memberTest/JoinTest.java
@@ -3,7 +3,6 @@ package kr.co.dream.memberTest;
 import javax.inject.Inject;
 
 import org.junit.Test;
-import org.junit.internal.runners.JUnit4ClassRunner;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -12,38 +11,38 @@ import kr.co.dream.member.domain.MemberVO;
 import kr.co.dream.member.persistence.MemberDAO;
 import kr.co.dream.member.service.MemberService;
 
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/root-context.xml" })
 
-public class joinTest {
-	
+public class JoinTest {
+
 	@Inject
 	private MemberDAO dao;
-	
-	@Inject MemberService service;
+
+	@Inject
+	MemberService service;
 
 	MemberVO member = new MemberVO();
-	
-	//@Test
-	public void voToStringTest(){
+
+	// @Test
+	public void voToStringTest() {
 		member.setMember_mail("rcn115@naver.com");
 		member.setMember_name("최용석");
 		member.setMember_stop_state("F");
-		
+
 		System.out.println(member.toStringStopMember());
-		
+
 	}
-	
+
 	@Test
-	public void joinTest(){
+	public void joinTest() {
 		member.setMember_mail("rcn115@naver.com");
 		member.setMember_pass("000");
 		member.setMember_name("최용석");
 		member.setMember_birth("910602");
 		member.setMember_gender("M");
-		
-//		dao.memberJoin(member);
+
+		// dao.memberJoin(member);
 		service.memberJoin(member);
 	}
 }
