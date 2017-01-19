a45d83d4267aaa59ce37b313e3e527a99e557951
Cys <rcn115@naver.com>
2017-01-19 18:04:09 +0900

    intercepter테스트 클래스 및 뷰 삭제 manager / qna 테이블 수정사항 적용

--- Dream/src/main/resources/table/dream_QNA.sql
+++ Dream/src/main/resources/table/dream_QNA.sql
@@ -6,12 +6,11 @@ DROP TABLE Dream_QNA;
 -- 질문 게시판
 CREATE TABLE Dream_QNA (
 	QNA_No       INTEGER       primary key, -- 질문 번호
-	Manager_Mail VARCHAR2(40)  NOT NULL REFERENCES Dream_Manager(Manager_Mail), -- 관리자 이메일
-	Member_Mail  VARCHAR2(40)  NOT NULL, -- 회원 이메일
+	Manager_Mail VARCHAR2(40)  REFERENCES Dream_Manager(Manager_Mail), -- 관리자 이메일
+	Member_Mail  VARCHAR2(40)  NOT NULL REFERENCES Dream_Member(Member_Mail), -- 회원 이메일
 	Member_Name  VARCHAR2(50)  NOT NULL, -- 작성자
 	QNA_Title    VARCHAR2(300) NOT NULL, -- 질문 제목
-	QNA_Text     LONG      NOT NULL, -- 질문 내용
-	QNA_Category VARCHAR2(30)  NOT NULL, -- 질문 카테고리
+	QNA_Content     LONG      NOT NULL, -- 질문 내용
 	QNA_Regdate  DATE          DEFAULT sysdate, -- 질문 작성일자
 	QNA_Check    VARCHAR2(10)  DEFAULT 'T' -- 질문 확인 여부
 );
@@ -23,3 +22,19 @@ create sequence dream_qna_seq
    nocycle
    nocache;
    
+   
+   insert into dream_qna 
+   (qna_no, Manager_Mail, Member_Mail, Member_Name, 
+   QNA_Title, QNA_Content)
+   values
+   (dream_qna_seq.nextval, 'admin@dream.com', 'test@naver.com', '최용석',
+   'qna 제목 테스트', 'qna 내용 테스트')
+
+   insert into dream_qna 
+   (qna_no,  Member_Mail, Member_Name, 
+   QNA_Title, QNA_Content)
+   values
+   (dream_qna_seq.nextval,  'test@naver.com', '최용석',
+   'qna 제목 테스트', 'qna 내용 테스트')
+   
+   select * from dream_qna
--- Dream/src/main/resources/table/dream_manager.sql
+++ Dream/src/main/resources/table/dream_manager.sql
@@ -4,11 +4,17 @@ DROP TABLE Dream_Manager ;
 
 -- 관리자
 CREATE TABLE Dream_Manager (
-	Manager_No    INTEGER      NOT NULL, -- 관리자 번호
-	Manager_Mail  VARCHAR2(40) primary key, -- 관리자 이메일
-	Manager_Grade VARCHAR2(10) DEFAULT 'GM', -- 관리자 등급
-	Active_Date	  TIMESTAMP	   NOT NULL
+	Manager_No    INTEGER       NOT NULL, -- 관리자 번호
+	Manager_Mail  VARCHAR2(40)  primary key, -- 관리자 이메일
+	Manager_Pass  VARCHAR2(100) NOT NULL,
+	Manager_Grade VARCHAR2(10)  DEFAULT 'GM', -- 관리자 등급
+	Active_Date	  TIMESTAMP	    NOT NULL
 );
+
+-- 관리자
+ALTER TABLE Dream_Manager
+	ADD CONSTRAINT Manager_Grade -- 관리자 등급 제약 사항
+		CHECK (Manager_Grade in ('GM' ,'CM'));
 
 drop sequence dream_manager_seq;
 create sequence dream_manager_seq
@@ -16,3 +22,8 @@ create sequence dream_manager_seq
 	increment by 1
 	nocycle
 	nocache;
+	
+
+	
+insert into dream_manager (manager_no, manager_mail, manager_pass, manager_grade, active_date) 
+					values (dream_manager_seq.nextval, 'admin@dream.com', '0000', 'CM', sysdate)
