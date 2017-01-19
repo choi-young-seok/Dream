15609b37a175a91a7a61988cb72173ae9b33b211
Cys <rcn115@naver.com>
2017-01-19 18:06:02 +0900

    SessionAuthIntercepter 작성 완료 및 servlet-context.xml등록 처리 완료 세션검사 요청 등록 가능 및 테스팅 완료

--- Dream/src/main/java/kr/co/dream/common/intercepter/SessionAuthIntercepter.java
+++ Dream/src/main/java/kr/co/dream/common/intercepter/SessionAuthIntercepter.java
@@ -2,7 +2,10 @@ package kr.co.dream.common.intercepter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import org.springframework.ui.ModelMap;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class SessionAuthIntercepter extends HandlerInterceptorAdapter {
@@ -11,9 +14,30 @@ public class SessionAuthIntercepter extends HandlerInterceptorAdapter {
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		// TODO Auto-generated method stub
-		return super.preHandle(request, response, handler);
+		
+		if (request.getSession().getAttribute("session") == null) {
+			request.getSession().setAttribute("needLoginSession","needLoginSession");
+			return true;
+		}
+		
+		return true;
 	}
+	
+	
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		HttpSession session = request.getSession();
+		String sessionValue =(String) session.getAttribute("needLoginSession");
 
-
+		System.out.println("SessionAuthIntercepter [ afterCompletion() session value ] : " + session.getAttribute("needLoginSession"));
+		if(sessionValue.equals("needLoginSession")){
+			session.invalidate();
+			System.out.println("SessionAuthIntercepter [ afterCompletion() ] 로그인을 위한 임시 세션 삭제");
+		}
+	
+		super.afterCompletion(request, response, handler, ex);
+	}
+	
+	
 }
